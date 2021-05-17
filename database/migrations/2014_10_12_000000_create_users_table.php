382a70e098c165f83cc8aee2daa026c33531d3d7
Salim66 <salimhasanriad8@gmail.com>
2021-05-17 21:17:01 +0600

    Setup user table and migrate user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('user_type')->default("User");
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->string('pincode')->nullable();
+            $table->string('mobile')->nullable();
+            $table->unsignedInteger('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
