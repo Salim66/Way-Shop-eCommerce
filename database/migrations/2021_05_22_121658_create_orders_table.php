ff5af9d2df96465486bca4bd530219f69fa6f393
Salim66 <salimhasanriad8@gmail.com>
2021-05-22 12:27:22 +0600

    Create order and orderproduct modal and migration

---
+++ database/migrations/2021_05_22_121658_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('user_email');
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('pincode');
+            $table->string('mobile');
+            $table->double('shipping_charges')->default(0);
+            $table->string('coupon_code')->nullable();
+            $table->double('coupon_amount')->nullable();
+            $table->string('order_status');
+            $table->string('payment_method');
+            $table->double('grand_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
